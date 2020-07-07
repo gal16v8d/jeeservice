c7fd83d93d02be7bcfde016e73fffa310383778d
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-07-06 20:45:24 -0500

    Update dependencies

--- jeeservice-rest-impl/src/main/java/co/com/gsdd/jeeservice/rest/api/impl/PersonRestImpl.java
+++ jeeservice-rest-impl/src/main/java/co/com/gsdd/jeeservice/rest/api/impl/PersonRestImpl.java
@@ -1,5 +1,8 @@
 package co.com.gsdd.jeeservice.rest.api.impl;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import co.com.gsdd.jeeservice.model.Person;
 import co.com.gsdd.jeeservice.rest.api.PersonRest;
 
@@ -7,10 +10,15 @@ public class PersonRestImpl implements PersonRest {
 
     @Override
     public Person getPersonById(String personId) {
+        Person p = generatePerson();
+        return Objects.equals(personId, p.getIdNumber()) ? p : null;
+    }
+
+    private Person generatePerson() {
         Person p = new Person();
-        p.setIdNumber("1234560789");
-        p.setName("Alex");
-        p.setLastName("Galvis");
+        p.setIdNumber("123456");
+        p.setName(UUID.randomUUID().toString());
+        p.setLastName(UUID.randomUUID().toString());
         return p;
     }
 
